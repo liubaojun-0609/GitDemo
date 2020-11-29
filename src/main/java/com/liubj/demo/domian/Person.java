1aada224f28e5e99167420921422036a93d34562
liubaojun-0609 <lbj0609@163.com>
2020-11-29 19:08:48 +0800

    创建dev分支 新增构造方法、getter/setter方法

--- src/main/java/com/liubj/demo/domian/Person.java
+++ src/main/java/com/liubj/demo/domian/Person.java
@@ -4,4 +4,28 @@ public class Person {
     private String name;
     private String addr;
     private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
